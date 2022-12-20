bb1fc0c1f2d41f954d10dae223b2620b3d64d760
thafran <108288654+thafran@users.noreply.github.com>
2022-12-20 09:47:57 +0100

    Update DysisBackground.ts
    Removed option for user to signup manually, user is now detected automatically.

--- client/src/background/DysisBackground.ts
+++ client/src/background/DysisBackground.ts
@@ -1,13 +1,13 @@
-import DysisBackgroundSync from './DysisBackgroundSync';
+import DysisBackgroundSync from "./DysisBackgroundSync";
+import { DysisRequest } from "../DysisRequest";
 
 export default class DysisBackground {
-
   private dysisParticipantID: string;
   private dysisParticipantAgreedToTerms: boolean = false;
   private dysisParticipantSubmitted: boolean = false;
 
   constructor() {
-    console.log('Dysis background script initiated ...')
+    console.log("Dysis background script initiated ...");
     this.setDefaultValues();
     this.onInstalled();
     this.getLocalStorageValuesAndInitSync();
@@ -18,67 +18,103 @@ export default class DysisBackground {
   private onInstalled() {
     // On installation event listener listening for installation event
     chrome.runtime.onInstalled.addListener(() => {
-      console.log('Dysis extension successfully installed ...')
+      console.log("Dysis extension successfully installed ...");
 
-      // Get local storage values to check if participant already takes part in study 
+      // Get local storage values to check if participant already takes part in study
       // which means they agreed to terms and submitted in the options panel
       chrome.storage.local.get(
-      [
-        'dysisInstallationDate',
-        'dysisParticipantAgreedToTerms',
-        'dysisParticipantSubmitted',
-      ], (res) => {
-        // If there is no installation date set (because never installed or removed and 
-        // installed instead of simple re-install) set installation date
-        if (!('dysisInstallationDate' in res)) {
-          chrome.storage.local.set({
-            dysisInstallationDate: new Date().toISOString(),
-          });
+        [
+          "dysisInstallationDate",
+          "dysisParticipantAgreedToTerms",
+          "dysisParticipantSubmitted",
+        ],
+        async (res) => {
+          try {
+            // If there is no installation date set (because never installed or removed and
+            // installed instead of simple re-install) set installation date
+            if (!("dysisInstallationDate" in res)) {
+              chrome.storage.local.set({
+                dysisInstallationDate: new Date().toISOString(),
+              });
+            }
+            // If user has never before installed the plugin, check for exisiting information. If not, create user
+            // and in database. To reenable the manual participant signup, uncomment
+            // chrome.tabs.create below and delete the new user creation routine.
+            if (
+              !res.dysisParticipantAgreedToTerms &&
+              !res.dysisParticipantSubmitted
+            ) {
+              const response = await DysisRequest.post("tracking/create", {
+                participantFirstName: "USER",
+                participantLastName: "USER",
+                participantAgreedToTerms: true,
+                participantSubmitted: true,
+                participantInstallationDate: new Date().toISOString(),
+              });
+              chrome.storage.local.set({
+                dysisParticipantFirstName: "USER",
+                dysisParticipantLastName: "USER",
+                dysisParticipantID: response.data.participantID,
+                dysisParticipantAgreedToTerms: true,
+                dysisParticipantSubmitted: true,
+                DysisInstallationDate: new Date().toISOString(),
+              });
+              /* chrome.tabs.create(
+              { url: `chrome-extension://${chrome.runtime.id}/options.html` },
+              () => {}
+            ); */
+            }
+          } catch (error) {
+            console.error("Error in usercreation check");
+          }
         }
-        // If participant does not yet take part in study and (re-) installs extension, 
-        // open options view to remind them of signing up
-        if (!res.dysisParticipantAgreedToTerms && !res.dysisParticipantSubmitted) {
-          chrome.tabs.create(
-            { url: `chrome-extension://${chrome.runtime.id}/options.html` },
-            () => {}
-          );
-        }
-      })
-    })
+      );
+    });
   }
 
   private setDefaultValues() {
     chrome.storage.local.get(
       [
-        'dysisParticipantFirstName',
-        'dysisParticipantLastName',
-        'dysisParticipantID',
-        'dysisParticipantAgreedToTerms',
-        'dysisParticipantSubmitted',
-      ], (res) => {
-      chrome.storage.local.set({
-        dysisParticipantFirstName: 
-          'dysisParticipantFirstName' in res ? res.dysisParticipantFirstName : '',
-        dysisParticipantLastName:
-          'dysisParticipantLastName' in res ? res.dysisParticipantLastName : '',
-        dysisParticipantID:
-          'dysisParticipantID' in res ? res.dysisParticipantID : null,
-        dysisParticipantAgreedToTerms:
-          'dysisParticipantAgreedToTerms' in res ? res.dysisParticipantAgreedToTerms : false,
-        dysisParticipantSubmitted:
-          'dysisParticipantSubmitted' in res ? res.dysisParticipantSubmitted : false,
-      })
-    });
+        "dysisParticipantFirstName",
+        "dysisParticipantLastName",
+        "dysisParticipantID",
+        "dysisParticipantAgreedToTerms",
+        "dysisParticipantSubmitted",
+      ],
+      (res) => {
+        chrome.storage.local.set({
+          dysisParticipantFirstName:
+            "dysisParticipantFirstName" in res
+              ? res.dysisParticipantFirstName
+              : "",
+          dysisParticipantLastName:
+            "dysisParticipantLastName" in res
+              ? res.dysisParticipantLastName
+              : "",
+          dysisParticipantID:
+            "dysisParticipantID" in res ? res.dysisParticipantID : null,
+          dysisParticipantAgreedToTerms:
+            "dysisParticipantAgreedToTerms" in res
+              ? res.dysisParticipantAgreedToTerms
+              : false,
+          dysisParticipantSubmitted:
+            "dysisParticipantSubmitted" in res
+              ? res.dysisParticipantSubmitted
+              : false,
+        });
+      }
+    );
   }
 
   private getLocalStorageValuesAndInitSync() {
     chrome.storage.local.get(
       [
-        'dysisParticipantID',
-        'dysisParticipantAgreedToTerms',
-        'dysisParticipantSubmitted',
-      ], (res) => {
-        this.dysisParticipantID = res.dysisParticipantID
+        "dysisParticipantID",
+        "dysisParticipantAgreedToTerms",
+        "dysisParticipantSubmitted",
+      ],
+      (res) => {
+        this.dysisParticipantID = res.dysisParticipantID;
         this.dysisParticipantAgreedToTerms = res.dysisParticipantAgreedToTerms;
         this.dysisParticipantSubmitted = res.dysisParticipantSubmitted;
         // Notice: As receiving local storage values is asynchronous, everything that
@@ -94,40 +130,40 @@ export default class DysisBackground {
 
   private initSync() {
     if (this.dysisParticipantAgreedToTerms && this.dysisParticipantSubmitted) {
-      new DysisBackgroundSync(this.dysisParticipantID)
+      new DysisBackgroundSync(this.dysisParticipantID);
     }
   }
-  
+
   private createBrowserActivityStateDetector() {
     // Adds an event listener which will fire and set the browser activity state every time a
     // a state change is detected and send it to the active tab content script
-    chrome.idle.onStateChanged.addListener(
-      (browserActivityState) => {
-        chrome.tabs.query({active: true, currentWindow: true}, (tabs) => {
-          if (tabs[0]?.url?.match('https:\/\/.*.reddit.com\/.*')) {
-            chrome.tabs.sendMessage(tabs[0].id, { browserActivityState: browserActivityState });
-          }
-        });
-      }
-    )
+    chrome.idle.onStateChanged.addListener((browserActivityState) => {
+      chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+        if (tabs[0]?.url?.match("https://.*.reddit.com/.*")) {
+          chrome.tabs.sendMessage(tabs[0].id, {
+            browserActivityState: browserActivityState,
+          });
+        }
+      });
+    });
   }
 
   private listenForLocalStorageChangesWhichRequireToReevaluteSync() {
     const callback = (
-      changes: chrome.storage.StorageChange, 
-      namespace: 'sync' | 'local' | 'managed'
+      changes: chrome.storage.StorageChange,
+      namespace: "sync" | "local" | "managed"
     ) => {
       for (let [key] of Object.entries(changes)) {
-        if (namespace === 'local' && key === 'dysisParticipantSubmitted') {
+        if (namespace === "local" && key === "dysisParticipantSubmitted") {
           this.getLocalStorageValuesAndInitSync();
         }
       }
-    }
+    };
     chrome.storage.onChanged.addListener((changes, namespace) => {
-      callback(changes, namespace)
+      callback(changes, namespace);
     });
   }
-  
+
   private debugDisplayMutationRecords() {
     chrome.storage.onChanged.addListener(function (changes, namespace) {
       for (let [key, { oldValue, newValue }] of Object.entries(changes)) {
